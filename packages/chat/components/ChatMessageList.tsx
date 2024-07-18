c830c2ab6b737e347d88b067d1804d154283b80c
Hao Tang <haotang.io@gmail.com>
2024-07-18 21:09:12 +0700

    feat: real messages

--- packages/chat/components/ChatMessageList.tsx
+++ packages/chat/components/ChatMessageList.tsx
@@ -1,28 +1,7 @@
 import * as React from "react";
 import {Box, Stack} from "@mui/material";
 
-const messages = [
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-  {message: "My company is a software component that are looking for strategic partner in oversea markets to act as sales and engagement directors to find and follow with clients in each market. ", sender: "user"},
-  {message: "We are looking for partners who are interested in building a long-term relationship with us. ", sender: "user"},
-];
-
-export function ChatMessageList() {
+export function ChatMessageList({messages}) {
   return (
     <Stack sx={{height: '100%', overflowY: 'scroll'}}>
       {messages.map(({message}, index) => (
--- packages/chat/components/ChatWindow.tsx
+++ packages/chat/components/ChatWindow.tsx
@@ -1,18 +1,21 @@
-import * as React from "react";
+import {useState} from "react";
 import {Box, Container} from "@mui/material";
 import {askChatGPT} from "../../../src/app/actions";
 import {ChatMessageList} from "../components/ChatMessageList";
 import {PromptInput} from "../components/PromptInput";
 
 export function ChatWindow() {
+  const [messages, setMessages] = useState([]);
+
   return (
     <Container sx={{flex: 1, display: 'flex', flexDirection: 'column'}}>
       <Box sx={{flexGrow: 1}}>
-        <ChatMessageList />
+        <ChatMessageList messages={messages} />
       </Box>
       <PromptInput onSubmit={async ({message}) => {
         const {response} = await askChatGPT({message});
-        console.log('response', response);
+        console.log(response);
+        setMessages([...messages, {message: response}]);
       }} />
     </Container>
   )
