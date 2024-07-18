c3870d0c300ab2de7bcebf5b042b9925bf637279
Hao Tang <haotang.io@gmail.com>
2024-07-18 18:20:04 +0700

    feat: better layout

--- packages/user/layouts/UserLayout.tsx
+++ packages/user/layouts/UserLayout.tsx
@@ -1,8 +1,126 @@
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import Divider from '@mui/material/Divider';
+// import Drawer from '@mui/material/Drawer';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import MailIcon from '@mui/icons-material/Mail';
+// import Toolbar from '@mui/material/Toolbar';
+// import {Navbar} from "../components";
+//
+// const drawerWidth = 240;
+//
+// interface Props extends Readonly<{
+//   children: React.ReactNode;
+// }> {
+//   /**
+//    * Injected by the documentation to work in an iframe.
+//    * Remove this when copying and pasting into your project.
+//    */
+//   window?: () => Window;
+// }
+//
+// export function UserLayout(props: Props) {
+//   const { window, children } = props;
+//   const [mobileOpen, setMobileOpen] = React.useState(false);
+//   const [isClosing, setIsClosing] = React.useState(false);
+//
+//   const handleDrawerClose = () => {
+//     setIsClosing(true);
+//     setMobileOpen(false);
+//   };
+//
+//   const handleDrawerTransitionEnd = () => {
+//     setIsClosing(false);
+//   };
+//
+//   const handleDrawerToggle = () => {
+//     if (!isClosing) {
+//       setMobileOpen(!mobileOpen);
+//     }
+//   };
+//
+//   const drawer = (
+//     <div>
+//       <Toolbar />
+//       <Divider />
+//       <List>
+//         {['Chat GPT 3.5', 'Chat GPT 4', 'Claude 2', 'Gemini 3', 'Dall-E 3', 'Sora'].map((text, index) => (
+//           <ListItem key={text} disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+//               </ListItemIcon>
+//               <ListItemText primary={text} />
+//             </ListItemButton>
+//           </ListItem>
+//         ))}
+//       </List>
+//     </div>
+//   );
+//
+//   // Remove this const when copying and pasting into your project.
+//   const container = window !== undefined ? () => window().document.body : undefined;
+//
+//   return (
+//     <Box sx={{ display: 'flex' }}>
+//       <Navbar handleDrawerToggle={handleDrawerToggle} />
+//       <Box
+//         component="nav"
+//         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+//         aria-label="mailbox folders"
+//       >
+//         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+//         <Drawer
+//           container={container}
+//           variant="temporary"
+//           open={mobileOpen}
+//           onTransitionEnd={handleDrawerTransitionEnd}
+//           onClose={handleDrawerClose}
+//           ModalProps={{
+//             keepMounted: true, // Better open performance on mobile.
+//           }}
+//           sx={{
+//             display: { xs: 'block', sm: 'none' },
+//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+//           }}
+//         >
+//           {drawer}
+//         </Drawer>
+//         <Drawer
+//           variant="permanent"
+//           sx={{
+//             display: { xs: 'none', sm: 'block' },
+//             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+//           }}
+//           open
+//         >
+//           {drawer}
+//         </Drawer>
+//       </Box>
+//       <Box
+//         component="main"
+//         sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, minHeight: '100vh' }}
+//       >
+//         <Toolbar />
+//         {children}
+//       </Box>
+//     </Box>
+//   );
+// }
+
 import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
@@ -10,8 +128,9 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
-import {Navbar} from "../components";
+import Typography from '@mui/material/Typography';
 
 const drawerWidth = 240;
 
@@ -46,8 +165,12 @@ export function UserLayout(props: Props) {
   };
 
   const drawer = (
-    <div>
-      <Toolbar />
+    <Box>
+      <Toolbar>
+        <Typography variant="h6">
+          Aiffordable
+        </Typography>
+      </Toolbar>
       <Divider />
       <List>
         {['Chat GPT 3.5', 'Chat GPT 4', 'Claude 2', 'Gemini 3', 'Dall-E 3', 'Sora'].map((text, index) => (
@@ -61,7 +184,46 @@ export function UserLayout(props: Props) {
           </ListItem>
         ))}
       </List>
-    </div>
+      <Divider />
+      <List>
+        {['Users', 'AI Tools'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['General Settings', 'Email settings'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['Plugins', 'About'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 
   // Remove this const when copying and pasting into your project.
@@ -69,7 +231,31 @@ export function UserLayout(props: Props) {
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <Navbar handleDrawerToggle={handleDrawerToggle} />
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="transparent"
+        elevation={0}
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Chat GPT 4
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -105,7 +291,7 @@ export function UserLayout(props: Props) {
       </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, width: { sm: `calc(100% - ${drawerWidth}px)` }, minHeight: '100vh' }}
+        sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
       >
         <Toolbar />
         {children}
