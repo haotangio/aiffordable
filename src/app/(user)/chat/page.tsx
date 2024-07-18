72f24babab8f3a74f49bbd4adc3f92f9798276d5
Hao Tang <haotang.io@gmail.com>
2024-07-18 22:13:54 +0700

    feat: add max duration

--- src/app/(user)/chat/page.tsx
+++ src/app/(user)/chat/page.tsx
@@ -4,6 +4,8 @@ import {Box} from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
 import {ChatWindow} from "@aiffordable/chat/components";
 
+export const maxDuration = 60;
+
 export default function ChatPage() {
   return (
     <Box sx={{minHeight: '100vh', flex: 1, display: 'flex', flexDirection: 'column'}}>
